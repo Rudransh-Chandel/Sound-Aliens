424f54ae5f5257d8ed15c30047941ce8c9c169e1
Rudransh-Chandel <106671046+Rudransh-Chandel@users.noreply.github.com>
2023-08-22 20:42:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 https://teachablemachine.withgoogle.com/models/NkAnAalZt/
 
+function start()
+{
+    navigator.mediaDevices.getUserMedia({audio: true});
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/NkAnAalZt/model.json", modelReady);
+}
+
+function modelReady()
+{
+    classifier.classify(result);
+}
+
+function result(error,results)
+{
+    if (error)
+    {
+        console.error(error);
+    }
+    else
+    {
+      console.log(results);
+    }
+}
